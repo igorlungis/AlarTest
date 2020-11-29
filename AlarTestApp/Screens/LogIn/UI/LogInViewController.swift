75e2f13641d51cdc1246e9652c27817bcb5c4d54
Igor Lungis <lungis@goozix.com>
2020-11-29 12:39:49 +0300

    [*] Log in UI implemented

---
+++ AlarTestApp/Screens/LogIn/UI/LogInViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LogInViewController.swift
+//  AlarTestApp
+//
+//  Created by Igor Lungis on 11/29/20.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+  
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var nameTF: UITextField!
+  @IBOutlet weak var passwordTF: UITextField!
+  @IBOutlet weak var nextButton: UIButton!
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+  @IBAction func nextDidTap(_ sender: Any) {
+    
+  }
+  
+
+}
